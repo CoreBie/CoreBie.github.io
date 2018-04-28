1dbb0410d79ebb72c0743d4d8e65ea6adfdc1d75
Corbie <wizardd2525@gmail.com>
2018-04-28 20:52:22 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function()
 
-particlesJS.load('particles-js', '/assets/particles/particlesjs-config.json', function()
+particlesJS.load('particles-js', '/media/assets/particles/particlesjs-config.json', function()
   console.log('callback - particles.js config loaded');
  
  );
